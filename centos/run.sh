5e632a0d7fd3edf0896c6f54a95e1ec75699a2ce
William Cannon <william.cannon@gmail.com>
2017-12-03 08:39:48 -0600

    nice

--- centos/run.sh
+++ centos/run.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Configure master
-#sed -i "s/#master: salt/master: localhost/g" /etc/salt/minion
-
-# Start the first process
-#/usr/bin/salt-master -d
-#status=$?
-#if [ $status -ne 0 ]; then
-#  echo "Failed to start salt-master: $status"
-#  exit $status
-#fi
-
-#sleep 5
-
-# Start the second process
 /usr/bin/salt-minion -d
 status=$?
 if [ $status -ne 0 ]; then
@@ -21,18 +7,9 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
-sleep 30
-
-#salt-key --yes --accept-all
-
 while /bin/true; do
-#  ps aux |grep salt-master |grep -q -v grep
-#  PROCESS_1_STATUS=$?
   ps aux |grep salt-minion |grep -q -v grep
   PROCESS_2_STATUS=$?
-  # If the greps above find anything, they will exit with 0 status
-  # If they are not both 0, then something is wrong
-#  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
   if [ $PROCESS_2_STATUS -ne 0 ]; then
     echo "The salt-minion process has already exited."
     exit -1
